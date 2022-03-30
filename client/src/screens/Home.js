e556dd72ca2ae2d0f6c8f829f56bd07ba81e1d97
Biswajit Ghosh <ghoshbiswajit2406@gmail.com>
2022-03-30 21:44:32 +0530

    Fetching Data from mongoDB

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import pizzas from '../pizzasdata'
+//import pizzas from '../pizzasdata'
 import { useState, useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import Pizza from '../components/Pizza'
@@ -8,21 +8,30 @@ import { getAllPizzas } from '../actions/pizzaActions'
 export default function Home() {
 
     const dispatch = useDispatch()
+    const pizzasstate=useSelector((state)=> state.getAllPizzasReducers)
+    const{pizzas, error, loading}=pizzasstate
     useEffect(() => {
         dispatch(getAllPizzas())
-    })
+    }, [])
+    
     
     return (
         <div>
             <div style={{margin: '25px'}} className='row' >
 
-                {pizzas.map(pizza => {
-                    return <div className="col-md-4 p-3 ">
-                        <div className='m-3 shadow-lg p-3 mb-5 bg-white rounded'>
-                            <Pizza pizza={pizza} />
+
+                {loading? <h1>Loading</h1> :error? <h1>Error</h1>: (
+                    pizzas.map(pizza => {
+                        return (
+                        <div className="col-md-4 p-3 ">
+                            <div key={pizza.id} className='m-3 shadow-lg p-3 mb-5 bg-white rounded'>
+                                <Pizza pizza={pizza} />
+                            </div>
                         </div>
-                    </div>
-                })}
+                    )
+                    })
+
+                )}
 
             </div>
         </div>
